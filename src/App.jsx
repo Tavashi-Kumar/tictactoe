40fcd2b4182ea9cbcc7fb773f9d8f121230f1b06
Tavashi Kumar <tavashi123@gmail.com>
2022-11-22 19:38:59 +0530

    added states,events and winner calc logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import Board from './components/Board'
-import "./styles/root.scss"
+import { useState } from 'react';
+import Board from './components/Board';
+import './styles/root.scss';
+import { calcWinner } from './helper';
 const App = () => {
+  const [board, updateBoardState] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(false);
+
+  const winner = calcWinner(board);
+
+  const message = winner
+    ? `Winner is ${winner}`
+    : `Next player : ${isXNext ? 'X' : 'O'}`;
+
+  const handleSquareClick = position => {
+    if (board[position] != null || winner) {
+      return;
+    }
+
+    updateBoardState(prevBoardState => {
+      //console.log(prevBoardState);
+      return prevBoardState.map((square_val, pos) => {
+        //console.log(square_val);
+        if (pos == position) {
+          return isXNext ? 'X' : 'O';
+        }
+        return square_val;
+      });
+    });
+    setIsXNext(prevBoardState => !prevBoardState);
+  };
+
   return (
-    <div className='app'>
-      <h1>Tic-Tac-Toe</h1>
-      <Board />
-    </div>
+    <body>
+      <div className="app">
+        <h1>Tic-Tac-Toe</h1>
+        <h3>{message}</h3>
+        <Board board={board} handleSquareClick={handleSquareClick} />
+      </div>
+    </body>
   );
 };
 
